1f13a6b7d89c44cca38311c477b862a54078723a
Scott Reilly <scott@coffee2code.com>
2010-09-09 15:00:52 -0400

    Release v2.0:
    * Re-implemented entire approach
    * Allow overriding of custom field used via 'silent_publish_meta_key' filter
    * Add class of 'c2c-silent-publish' to admin UI div containing checkbox
    * Add filter 'silent_publish_meta_key' to allow overriding custom field key name
    * Remove function `add_js()`, `admin_menu()`, `add_meta_box()`
    * Add functions `init()`, `add_ui()`, `save_silent_publish_status()`, `load_textdomain()`
    * Add true localization support
    * Move definition of strings into `init()` and properly support localization
    * Full support for localization
    * Store plugin instance in global variable, `$c2c_silent_publish`, to allow for external manipulation
    * Remove docs from top of plugin file (all that and more are in readme.txt)
    * Minor code reformatting (spacing)
    * Add PHPDoc documentation
    * Note compatibility with WP 2.9+, 3.0+
    * Drop compatibility with versions of WP older than 2.9
    * Update screenshots
    * Update copyright date
    * Add package info to top of plugin file
    * Add Changelog, Frequently Asked Questions, Filters, and Upgrade Notice sections to readme.txt
    * Add .pot file
    * Add to plugin repository

--- silent-publish.php
+++ silent-publish.php
@@ -1,46 +1,32 @@
 <?php
+/**
+ * @package Silent_Publish
+ * @author Scott Reilly
+ * @version 2.0
+ */
 /*
 Plugin Name: Silent Publish
-Version: 1.0
-Plugin URI: http://coffee2code.com/wp-plugins/silent-publish
+Version: 2.0
+Plugin URI: http://coffee2code.com/wp-plugins/silent-publish/
 Author: Scott Reilly
 Author URI: http://coffee2code.com
+Text Domain: silent-publish
 Description: Adds the ability to publish a post without triggering pingbacks, trackbacks, or notifying update services.
 
-This plugin adds a "Publish silently" checkbox to the "Write Post" admin page.  If checked when the post is published, that
-post will not trigger the pingbacks, trackbacks, and update service notifications that might typically occur.
+Compatible with WordPress 2.9+, 3.0+
 
-In every other manner, the post is published as usual: it'll appear on the front page, archives, and feeds as expected, and no
-other aspect of the post is affected.
-
-While trackbacks and pingsbacks can already be disabled from the Add New Post/Page page, this plugin makes things easier by allowing
-a single checkbox to disable those things, in addition to disabling notification of update services which otherwise could only
-be disabled by clearing the value of the global setting, which would then affect all authors and any subsequently published posts.
-
-If a post is silently published, a custom field '_silent_publish' for the post is set to a value of 1 as a means of recording the
-action.  However, this value is not then used for any purpose as of yet.  Nor is the custom field unset or changed if the post is
-later re-published.
-
-Compatible with WordPress 2.6+, 2.7+.
-
-=>> Read the accompanying readme.txt file for more information.  Also, visit the plugin's homepage
-=>> for more information and the latest updates
-
-Installation:
-
-1. Download the file http://coffee2code.com/wp-plugins/silent-publish.zip and unzip it into your 
-/wp-content/plugins/ directory.
-2. Activate the plugin through the 'Plugins' admin menu in WordPress
-3. Click the 'Publish silently' checkbox when publishing a post to prevent triggering of pingbacks, trackbacks, or notifications to update services.
+=>> Read the accompanying readme.txt file for instructions and documentation.
+=>> Also, visit the plugin's homepage for additional information and updates.
+=>> Or visit: http://wordpress.org/extend/plugins/silent-publish/
 
 */
 
 /*
-Copyright (c) 2009 by Scott Reilly (aka coffee2code)
+Copyright (c) 2009-2010 by Scott Reilly (aka coffee2code)
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
-files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
-modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
 Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -51,78 +37,110 @@ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRA
 IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-if ( !class_exists('SilentPublish') ) :
+if ( !class_exists( 'c2c_SilentPublish' ) ) :
 
-class SilentPublish {
+class c2c_SilentPublish {
 	var $field = 'silent_publish';
-	var $title = 'Silent Publish';
-	var $label = 'Publish silently';
-	var $help = 'If checked, upon publication of this post do not perform any pingbacks, trackbacks, or update service notifications.';
-	var $meta_key = '_silent_publish';
+	var $meta_key = '_silent-publish'; // Filterable via 'silent_publish_meta_key' filter
+	var $textdomain = 'silent-publish';
+	var $textdomain_subdir = 'lang';
 
-	function SilentPublish() {
-		$this->title = __($this->title);
-		$this->label = __($this->label);
-		$this->help = __($this->help);
+	/**
+	 * Constructor
+	 */
+	function c2c_SilentPublish() {
+		add_action( 'init', array( &$this, 'init' ) );
+	}
+
+	/**
+	 * Register actions/filters and allow for configuration
+	 *
+	 * @since 2.0
+	 *
+	 * @return void
+	 */
+	function init() {
+		$this->load_textdomain();
+
+		$this->meta_key = esc_attr( apply_filters( 'silent_publish_meta_key', $this->meta_key ) );
 		$this->meta_box = 'meta_box-' . $this->field;
 
-		if ( is_admin() ) {
-			add_action('admin_head', array(&$this, 'add_js'));
-			add_action('admin_menu', array(&$this, 'admin_menu'));
-		}
-		add_action('publish_post', array(&$this, 'publish_post'), 1, 1);
+		add_action( 'post_submitbox_misc_actions', array( &$this, 'add_ui' ) );
+		add_filter( 'wp_insert_post_data', array( &$this, 'save_silent_publish_status' ), 2, 2 );
+		add_action( 'publish_post', array( &$this, 'publish_post' ), 1, 1 );
 	}
 
-	function admin_menu() {
-		global $pagenow;
-		$post_ID = (int) $_GET['post'];
-		if ( $post_ID )
-			$post = get_post($post_ID);
-
-		// The silent publish capability is only exposed for a new post/page, or an existing post/page that is a draft or pending.
-		if ( in_array($pagenow, array('post-new.php', 'page-new.php')) ||
-			(!empty($post->ID) && in_array($post->post_status, array('draft', 'pending'))) ) {
-			add_meta_box($this->meta_box, $this->title, array(&$this, 'add_meta_box'), 'post', 'side');
-			add_meta_box($this->meta_box, $this->title, array(&$this, 'add_meta_box'), 'page', 'side');
-		}
+	/**
+	 * Loads the localization textdomain for the plugin.
+	 *
+	 * @since 2.0
+	 *
+	 * @return void
+	 */
+	function load_textdomain() {
+		$subdir = empty( $this->textdomain_subdir ) ? '' : '/'.$this->textdomain_subdir;
+		load_plugin_textdomain( $this->textdomain, false, basename( dirname( __FILE__ ) ) . $subdir );
 	}
 
-	// For those with JS enabled, the checkbox is moved into the Publish meta_box and the plugin's meta_box is hidden.
-	// The fallback for non-JS people is that the plugin's meta_box is shown and the checkbox can be found there.
-	function add_js() {
-		echo <<<JS
-		<script type="text/javascript">
-		jQuery(document).ready(function($) {
-			$('#div-{$this->field}').insertBefore($('.misc-pub-section-last'));
-			$('#{$this->meta_box}').hide();
-		});
-	
-		</script>
-JS;
+	/**
+	 * Draws the UI to prompt user if silent publish should be enabled for the post.
+	 *
+	 * @since 2.0
+	 *
+	 * @return void (Text is echoed.)
+	 */
+	function add_ui() {
+		global $post;
+		if ( $post->post_status == 'publish' )
+			return;
+		$value = get_post_meta( $post->ID, $this->meta_key, true );
+		$checked = checked( $value, '1', false );
+		echo "<div class='misc-pub-section'><label class='selectit c2c-silent-publish' for='{$this->field}' title='";
+		esc_attr_e( 'If checked, upon publication of this post do not perform any pingbacks, trackbacks, or update service notifications.', $this->textdomain );
+		echo "'>\n";
+		echo "<input id='{$this->field}' type='checkbox' $checked value='1' name='{$this->field}' />\n";
+		_e( 'Silent publish?', $this->textdomain );
+		echo '</label></div>' . "\n";
 	}
 
-	function add_meta_box() {
-		echo <<<HTML
-		<div id="div-{$this->field}" class="misc-pub-section"><label class="selectit" for="{$this->field}" title="{$this->help}"><input type="checkbox" value="open" id="{$this->field}" name="{$this->field}" /> {$this->label}</label></div>
-		<p>{$this->help}</p>
-HTML;
+	/**
+	 * Update the value of the silent publish custom field, but only if it is supplied.
+	 *
+	 * @since 2.0
+	 *
+	 * @param array $data Data
+	 * @param array $postarr Array of post fields and values for post being saved
+	 * @return array The unmodified $data
+	 */
+	function save_silent_publish_status( $data, $postarr ) {
+		$new_value = isset( $postarr[$this->field] ) ? $postarr[$this->field] : '';
+		update_post_meta( $postarr['ID'], $this->meta_key, $new_value );
+		return $data;
 	}
 
+	/**
+	 * Handles silent publishing if the associated checkbox is checked.
+	 *
+	 * Save the fact this post was silently published
+	 * This does not attempt to clear this value if the post later gets republished without silent publishing.
+	 * Also, this stored value is not currently used, merely saved.
+	 *
+	 * @since 1.0
+	 *
+	 * @param int $post_id Post ID
+	 * @return void
+	 */
 	function publish_post( $post_id ) {
-		if ( isset($_POST[$this->field]) && $_POST[$this->field] ) {
-			define('WP_IMPORTING', true);
-			// Save the fact this post was silently published
-			// This does not attempt to clear this value if the post later gets republished without silent publishing.
-			// Also, this stored value is not currently used, merely saved.
-			update_post_meta($post_id, $this->meta_key, 1);
+		if ( isset( $_POST[$this->field] ) && $_POST[$this->field] ) {
+			define( 'WP_IMPORTING', true );
+			update_post_meta( $post_id, $this->meta_key, 1 );
 		}
 	}
 
-} // end SilentPublish
+} // end c2c_SilentPublish
+
+$GLOBALS['c2c_silent_publish'] = new c2c_SilentPublish();
 
 endif; // end if !class_exists()
-
-if ( class_exists('SilentPublish') )
-	new SilentPublish();
 
 ?>
